47c04f3cb10c1097d88e8af3080efe66559ae23c
FagTI <guttlauncher@gmail.com>
2024-09-17 01:04:21 -0300

    :poop:

--- GeneticAlgorithm/1st-lab.py
+++ GeneticAlgorithm/1st-lab.py
@@ -2,10 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 p = 20
-n_bits_var = 10
+n_bits_var = 8
 N = 100
 epochs = 10
 tolerancia = 1e-6
+
+populacao_inicial = np.random.randint(low=0, high=2, size=(N, p * n_bits_var))
+print(f"População inicial: {populacao_inicial}")
 
 def f(x):
     A = 10
@@ -17,48 +20,41 @@ def f(x):
     
     return somatorio
 
-populacao_inicial = np.random.randint(low=0, high=2, size=(N, p * n_bits_var))
-print(f"População inicial: {populacao_inicial}")
-
-def decodifica_cromossomo(cromossomo, n_bits_per_var, intervalo=(-10, 10)):
-    p = len(cromossomo) // n_bits_per_var
-    variaveis = []
+def phi(cromossomo, intervalo=(-10, 10)):
+    s = 0
     
-    for i in range(p):
-        bits = cromossomo[i * n_bits_per_var:(i + 1) * n_bits_per_var]
-        valor_binario = int(''.join(bits.astype(str)), 2)
-        valor_real = intervalo[0] + (intervalo[1] - intervalo[0]) * (valor_binario / (2**n_bits_per_var - 1))
-        variaveis.append(valor_real)
+    for i in range(len(cromossomo)):
+        s += cromossomo[len(cromossomo) - i - 1] * 2**i
     
-    return variaveis
+    return intervalo[0] + (intervalo[1] - intervalo[0]) / (2**len(cromossomo) - 1) * s
 
 cromossomo_exemplo = populacao_inicial[0]
-variaveis_reais = decodifica_cromossomo(cromossomo_exemplo, n_bits_var)
+variaveis_reais = phi(cromossomo_exemplo)
 print("Variáveis Reais do Cromossomo Exemplo:\n", variaveis_reais)
 
 t = 0
-melhor_aptidao = float('inf')
-geracoes_sem_melhoria = 0
+# melhor_aptidao = float('inf')
+# geracoes_sem_melhoria = 0
 
-while t <= epochs:
-    aptidoes = []
+# while t <= epochs:
+#     aptidoes = []
     
-    for individuo in populacao_inicial:
-        variaveis_reais = decodifica_cromossomo(cromossomo=individuo, n_bits_per_var=n_bits_var)
-        aptidao = f(variaveis_reais)
-        aptidoes.append(aptidao)
+#     for individuo in populacao_inicial:
+#         variaveis_reais = phi(cromossomo=individuo)
+#         aptidao = f(variaveis_reais)
+#         aptidoes.append(aptidao)
     
-    melhor_aptidao_atual = min(aptidoes)
+#     melhor_aptidao_atual = min(aptidoes)
     
-    if abs(melhor_aptidao_atual - melhor_aptidao) < tolerancia:
-        geracoes_sem_melhoria += 1
-    else:
-        geracoes_sem_melhoria = 0
+#     if abs(melhor_aptidao_atual - melhor_aptidao) < tolerancia:
+#         geracoes_sem_melhoria += 1
+#     else:
+#         geracoes_sem_melhoria = 0
     
-    if geracoes_sem_melhoria >= 3:
-        print(f"Critério de parada atingido na geração {t}. Melhor aptidão: {melhor_aptidao_atual}")
-        break
+#     if geracoes_sem_melhoria >= 3:
+#         print(f"Critério de parada atingido na geração {t}. Melhor aptidão: {melhor_aptidao_atual}")
+#         break
     
-    melhor_aptidao = melhor_aptidao_atual
+#     melhor_aptidao = melhor_aptidao_atual
     
-    t += 1
+#     t += 1
